6b15d5e66d0cca626c3b7b8d999404340e408d6c
balta-frc <alptng72@gmail.com>
2025-02-16 21:08:23 +0300

    Create and organize necessary subsytems

--- src/main/java/frc/robot/subsystems/ElevatorSubsytem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsytem.java
@@ -6,47 +6,58 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import frc.robot.Constants;
 
 public class ElevatorSubsytem extends SubsystemBase {
 
-      public Pigeon2 gyro;
-      public TalonFX First_Elevator_Kraken;
-      public TalonFX Second_Elevator_Kraken;
-      public CANcoder Elevator_Cancoder;
-    
-      public PIDController elevator_PidController;
+  private SparkMax leftMax = new SparkMax(Constants.ElevatorConstants.LEFT_ELEVATOR_ID, MotorType.kBrushless);
+  private SparkMax rightMax = new SparkMax(Constants.ElevatorConstants.RIGHT_ELEVATOR_ID, MotorType.kBrushless);
+  private PIDController elevatorPidController = new PIDController(Constants.ElevatorConstants.KP, Constants.ElevatorConstants.KI, Constants.ElevatorConstants.KD);
+  private RelativeEncoder leftRelativeEncoder;
+  private RelativeEncoder rightRelativeEncoder;
+
+  private double setpoint = 0;
 
   /** Creates a new ElevatorSubsytem. */
   public ElevatorSubsytem() {
-
-    First_Elevator_Kraken = new TalonFX(Constants.First_ElevatorID, "Canivore");
-    Second_Elevator_Kraken = new TalonFX(Constants.Second_ElevatorID, "Canivore");
-    Elevator_Cancoder = new CANcoder(Constants.Elevator_CancoderID, "Canivore");
-
-    elevator_PidController = new PIDController(0, 0, 0);
-
-    StatusSignal<Angle> Elevator_AbsultePosition = Elevator_Cancoder.getAbsolutePosition();
-
+    leftRelativeEncoder = leftMax.getEncoder();
+    rightRelativeEncoder = rightMax.getEncoder();
   }
 
   @Override
   public void periodic() {
-
-
-
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Raw left encoder data(position):", leftRelativeEncoder. getPosition());
+    SmartDashboard.putNumber("Left encoder data(in meters):", leftRelativeEncoder.getPosition()*Constants.ElevatorConstants.POSITION_2_DISTANCE);
+    // Will be added the pid logic
   }
 
-  public void Update_Elevator_Status() {
+  public void setElevatorSetpoint(double setpoint){
+    this.setpoint = setpoint;
+  }
 
+  public void elevatorUp(){
+    leftMax.set(-0.3);
+    rightMax.set(-0.3);
+  }
 
+  public void elevatorDown(){
+    leftMax.set(0.3);
+    rightMax.set(0.3);
+  }
 
+  public void stop(){
+    leftMax.set(0);
+    rightMax.set(0);
   }
 
 }
