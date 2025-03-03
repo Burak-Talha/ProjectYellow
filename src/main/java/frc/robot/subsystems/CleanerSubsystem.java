b084c2ac0fa7ec5758eff92c6127a391d43f3d72
balta-frc <alptng72@gmail.com>
2025-03-03 22:26:07 +0300

    Add some methods

--- src/main/java/frc/robot/subsystems/CleanerSubsystem.java
+++ src/main/java/frc/robot/subsystems/CleanerSubsystem.java
@@ -4,32 +4,59 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class CleanerSubsystem extends SubsystemBase {
 
-  SparkMax cleaner = new SparkMax(35, MotorType.kBrushless);
+  SparkMax cleanerRot;
+  SparkMax cleanerPow;
+  RelativeEncoder rotRelativeEncoder;
 
   /** Creates a new Cleaner. */
-  public CleanerSubsystem() {}
+  public CleanerSubsystem() {
+    cleanerRot = new SparkMax(Constants.CleanerConstants.CLEANER_ROT_SPARKMAX_ID, MotorType.kBrushless);
+    cleanerPow = new SparkMax(Constants.CleanerConstants.CLEANER_POW_SPARKMAX_ID, MotorType.kBrushless);
+    rotRelativeEncoder = cleanerRot.getEncoder();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Get rotation degree :", getRotationDegree());
   }
 
   public void getIn(){
-    cleaner.set(0.5);
+    cleanerPow.set(0.5);
   }
 
   public void getOut(){
-    cleaner.set(-0.5);
+    cleanerPow.set(-0.5);
+  }
+
+  public double getRotationDegree(){
+    return getPosition()*Constants.CleanerConstants.POSITION_2_DEGREE;
+  }
+
+  public double getPosition(){
+    return rotRelativeEncoder.getPosition();
   }
 
   public void stopMotors(){
-    cleaner.set(0);
+    stopPowMotor();
+    stopRotMotor();
+  }
+
+  public void stopPowMotor(){
+    cleanerPow.stopMotor();
+  }
+
+  public void stopRotMotor(){
+    cleanerRot.stopMotor();
   }
 }
