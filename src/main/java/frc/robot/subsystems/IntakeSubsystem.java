1021297f33102c476a00ecf82a25356bbe1959db
balta-frc <alptng72@gmail.com>
2025-03-03 22:25:58 +0300

    Create IntakeSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakeSubsystem extends SubsystemBase {
+  SparkMax rotSparkMax;
+  SparkMax powSparkMax;
+  RelativeEncoder rotRelativeEncoder;
+
+  /** Creates a new IntakeSubsystem. */
+  public IntakeSubsystem() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    rotSparkMax = new SparkMax(Constants.IntakeConstants.INTAKE_ROT_SPARKMAX_ID, MotorType.kBrushless);
+    powSparkMax = new SparkMax(Constants.IntakeConstants.INTAKE_POW_SPARKMAX_ID, MotorType.kBrushless);  
+    rotRelativeEncoder = rotSparkMax.getEncoder();
+
+    // Set encoder value to start pose
+    rotRelativeEncoder.setPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Current intake degree position", getRotationDegree());
+  }
+
+  public void getIn(){
+    powSparkMax.set(0.5);
+  }
+
+  public void getOut(){
+    powSparkMax.set(-0.5);
+  }
+
+  public void stopPowerMotor(){
+    powSparkMax.stopMotor();
+  }
+
+  public void stopRotationMotor(){
+    rotSparkMax.stopMotor();
+  }
+
+  public double getRotationDegree(){
+    return getEncoderPosition()*Constants.IntakeConstants.POSITION_2_DEGREE;
+  }
+
+  private double getEncoderPosition(){
+    return rotRelativeEncoder.getPosition();
+  }
+}
