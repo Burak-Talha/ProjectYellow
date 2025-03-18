83721acc18d47c3ee6972477fef56294a6c5fc2a
balta-frc <alptng72@gmail.com>
2025-03-18 22:13:28 +0300

    Modify the folder root

--- src/main/java/frc/robot/commands/IntakeCommands/IntakeDownCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeDownCommand.java
@@ -2,18 +2,18 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.IntakeCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class OuttakeCommand extends Command {
-  
+public class IntakeDownCommand extends Command {
+
   IntakeSubsystem intakeSubsystem;
 
-  /** Creates a new OuttakeCommand. */
-  public OuttakeCommand(IntakeSubsystem intakeSubsystem) {
+    /** Creates a new IntakeUpCommand. */
+  public IntakeDownCommand(IntakeSubsystem intakeSubsystem) {
     this.intakeSubsystem = intakeSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(intakeSubsystem);
@@ -26,13 +26,14 @@ public class OuttakeCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intakeSubsystem.outtake();
+    intakeSubsystem.intakeDown();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intakeSubsystem.stopPowerMotor();
+
+    intakeSubsystem.stopRotationMotor();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/commands/IntakeCommands/OuttakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/OuttakeCommand.java
@@ -2,18 +2,18 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.IntakeCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class IntakeDownCommand extends Command {
-
+public class OuttakeCommand extends Command {
+  
   IntakeSubsystem intakeSubsystem;
 
-    /** Creates a new IntakeUpCommand. */
-  public IntakeDownCommand(IntakeSubsystem intakeSubsystem) {
+  /** Creates a new OuttakeCommand. */
+  public OuttakeCommand(IntakeSubsystem intakeSubsystem) {
     this.intakeSubsystem = intakeSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(intakeSubsystem);
@@ -26,14 +26,13 @@ public class IntakeDownCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intakeSubsystem.intakeDown();
+    intakeSubsystem.outtake();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
-    intakeSubsystem.stopRotationMotor();
+    intakeSubsystem.stopPowerMotor();
   }
 
   // Returns true when the command should end.
